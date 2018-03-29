1de41cd874534616a9fb6a8ddd108e55cc93591c
Johnny Tabangi <jtabangi@myseneca.ca>
2018-03-29 18:21:03 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer.component';
 import { NavComponent } from './nav.component';
 import { ContentComponent } from './content.component';
-import { FooterComponent } from './footer.component';
 import { HomeComponent } from './home.component';
 import { EmployeesComponent } from './employees.component';
 import { PositionsComponent } from './positions.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PositionService } from './data/position.service';
+import { EmployeeService } from './data/employee.service'; 
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
     NavComponent,
     ContentComponent,
-    FooterComponent,
     HomeComponent,
     EmployeesComponent,
     PositionsComponent,
@@ -27,7 +30,7 @@ import { PageNotFoundComponent } from './page-not-found.component';
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [PositionService, EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
