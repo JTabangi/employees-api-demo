5097206a94fc361ec3198c2fa272a5e450178636
Johnny Tabangi <jtabangi@myseneca.ca>
2018-03-30 15:20:25 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 05
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Johnny Tabangi Student ID: 100462167 Date: 2018-03-30
+*
+********************************************************************************/ 
+
 import { Component } from '@angular/core';
 
 @Component({
