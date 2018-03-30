ec1323873fca99f075ea13b7222af5a58eca324d
Johnny Tabangi <jtabangi@myseneca.ca>
2018-03-30 15:11:00 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PageNotFoundComponent } from './page-not-found.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PositionService } from './data/position.service';
 import { EmployeeService } from './data/employee.service'; 
+import { HttpClient } from '@angular/common/http/src/client';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { EmployeeService } from './data/employee.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [PositionService, EmployeeService],
   bootstrap: [AppComponent]
