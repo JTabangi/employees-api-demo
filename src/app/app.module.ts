2ff5de898d036b6d3121ed376ae133b0d397dd5f
Johnny Tabangi <jtabangi@myseneca.ca>
2018-03-30 15:32:08 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer.component';
 import { NavComponent } from './nav.component';
 import { ContentComponent } from './content.component';
 import { HomeComponent } from './home.component';
+import { FooterComponent } from './footer.component';
 import { EmployeesComponent } from './employees.component';
 import { PositionsComponent } from './positions.component';
 import { PageNotFoundComponent } from './page-not-found.component';
@@ -19,10 +19,10 @@ import { HttpClient } from '@angular/common/http/src/client';
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     NavComponent,
     ContentComponent,
     HomeComponent,
+    FooterComponent,
     EmployeesComponent,
     PositionsComponent,
     PageNotFoundComponent
